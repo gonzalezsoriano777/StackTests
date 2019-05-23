063950fb5bb14a82b9c26d32f7ac90dfc531a544
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-23 13:26:38 -0500

    Prac.

---
+++ StackTests/StackTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StackTests
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestMethod]
+        public void IsEmpty_EmptyStack_ReturnsTrue()
+        {
+            MyStack stack = new MyStack();
+            Assert.IsTrue(stack.IsEmpty);
+        }
+    }
+
+    public class MyStack
+    {
+        public bool IsEmpty
+        {
+            get { return true; }
+        }
+    }
+}
