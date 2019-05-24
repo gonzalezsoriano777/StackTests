a1e632880a4fc54d692a046f741177950209d400
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-05-23 19:19:30 -0500

    test

---
+++ StackTests/ImmutableStackTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StackTests
+{
+    [TestClass]
+    public class ImmutableStackTests
+    {
+        [TestMethod]
+        public void StackTests()
+        {
+        }
+    }
+}
